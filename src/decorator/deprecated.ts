0589dcd42093c0d3f771c3291fb7be20190c1e42
ShengJie <shengjie0202@gmail.com>
2019-10-22 19:08:17 +0800

    add deprecated example

--- src/decorator/deprecated.ts
+++ src/decorator/deprecated.ts
@@ -1,4 +1,21 @@
 
+/**
+ * Marking deprecated method
+ * 
+ * ```TypeScript
+ * class Foo {
+ *   @deprecated()
+ *   public fn1() {}
+ *
+ *   @deprecated('Use another api')
+ *   public fn2() {}
+ * }
+ * ```
+ *
+ * @export
+ * @param {string} [message]
+ * @returns
+ */
 export function deprecated(message?: string) {
   return (target: any, key: string | symbol, desp: PropertyDescriptor) => {
     const method = desp.value;
