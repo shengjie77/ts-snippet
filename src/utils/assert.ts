b7613670e6c754fdc155f5bc00627890979e4c4e
ShengJie <shengjie0202@outlook.com>
2020-06-20 15:21:15 +0800

    add assertNotNullable

--- src/utils/assert.ts
+++ src/utils/assert.ts
@@ -10,3 +10,9 @@ export function assert(val: boolean, message?: string): asserts val {
     throw new Error(message);
   }
 }
+
+export function assertNotNullable<T>(val: T | undefined | null, message?: string): asserts val is T {
+  if (val === undefined || val === null) {
+    throw new Error(message);
+  }
+}
